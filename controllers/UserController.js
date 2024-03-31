8b59b2444fcf903955a7eb9e2c2ee19866280eae
sonamsahu2909 <sonamsahu2909@gmail.com>
2024-03-31 10:23:18 +0530

    ok

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -43,7 +43,7 @@ class UserController {
       const salt = await bcrypt.genSalt(10);
       otp.otp = await bcrypt.hash(otp.otp, salt);
       const result = await otp.save();
-      console.log(result)
+      // console.log(result)
       return res.status(200).json({
         success: true,
         message: "Otp send successfully",
@@ -63,10 +63,10 @@ class UserController {
         return res.status(400).send("you are expired otp!");
       const rightOtpFind = otpHolder[otpHolder.length - 1];
       // console.log(rightOtpFind);
-      // const validUser = await bcrypt.compare(req.body.otp, rightOtpFind.otp);
-      // console.log(validUser)
-      // if (rightOtpFind.number === req.body.number && validUser) {
-        if (rightOtpFind.number === req.body.number ) {
+      const validUser = await bcrypt.compare(req.body.otp, rightOtpFind.otp);
+      console.log(validUser)
+      if (rightOtpFind.number === req.body.number && validUser) {
+        // if (rightOtpFind.number === req.body.number ) {
         const user = new UserModal(_.pick(req.body, ["number"]));
         const token = user.generateJWT();
         const result = await user.save();
